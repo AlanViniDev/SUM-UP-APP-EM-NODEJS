cee8b5fcb5ada12d6ba0649c3dab384979b18ba6
Alan Vinicius <alan123vinicius@gmail.com>
2020-06-16 02:54:48 -0300

    Add files via upload

---
+++ routes/route.controller.js
@@ -0,0 +1,18 @@
+// Constantes e rotas e requisições
+const express = require('express');
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express.Router();
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+// requisição do controller
+const view_controller =  require("../control/view.controller.js");
+const style_controller =  require("../control/style.controller.js");
+const operacoes_controller =  require("../control/operacoes.controller.js");
+
+//rota
+router.get('/index', view_controller.view);
+router.get('/styleCSS', style_controller.style);
+router.get('/operacoesJS', operacoes_controller.operacoes);
+module.exports = router;  
